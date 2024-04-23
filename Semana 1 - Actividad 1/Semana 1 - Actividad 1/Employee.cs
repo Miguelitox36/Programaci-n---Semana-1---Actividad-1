d86c1a9089cd6700eecd59db797b669fe080c7a8
Miguelitox36 <miguelitox36@hotmail.com>
2024-04-23 18:59:53 -0500

    Implementado Constructor y Metodo

---
+++ Semana 1 - Actividad 1/Semana 1 - Actividad 1/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_1___Actividad_1
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Employee(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+                
+        public virtual void PrintDetails()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+}
