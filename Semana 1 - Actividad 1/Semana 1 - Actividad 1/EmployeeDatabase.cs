91574cfa53511c78752e35471963d0071668390c
Miguelitox36 <miguelitox36@hotmail.com>
2024-04-23 19:03:55 -0500

    Implementación de métodos de la interfaz, If-else

---
+++ Semana 1 - Actividad 1/Semana 1 - Actividad 1/EmployeeDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_1___Actividad_1
+{
+    public class EmployeeDatabase : IEmployeeDatabase
+    {
+        private Dictionary<string, Employee> employees = new Dictionary<string, Employee>();
+                
+        public void AddEmployee(Employee employee)
+        {
+            employees.Add(employee.Name, employee);
+        }
+
+        public Employee GetEmployeeByName(string name)
+        {
+            if (employees.ContainsKey(name))
+                return employees[name];
+            else
+                return null;
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            return new List<Employee>(employees.Values);
+        }
+    }
+}
