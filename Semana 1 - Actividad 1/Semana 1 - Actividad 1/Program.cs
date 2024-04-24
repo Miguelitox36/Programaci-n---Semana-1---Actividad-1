b38ed0d90a36caecd3316be99d5c53e1868d050c
Miguelitox36 <miguelitox36@hotmail.com>
2024-04-23 19:09:11 -0500

    Implementación Lista / if-else

--- Semana 1 - Actividad 1/Semana 1 - Actividad 1/Program.cs
+++ Semana 1 - Actividad 1/Semana 1 - Actividad 1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Semana_1___Actividad_1;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        EmployeeDatabase database = new EmployeeDatabase();
+
+        
+        Employee emp1 = new Employee("John Doe", 30);
+        Manager manager1 = new Manager("Jane Smith", 35, "HR");
+
+        
+        database.AddEmployee(emp1);
+        database.AddEmployee(manager1);
+
+        
+        Employee foundEmployee = database.GetEmployeeByName("John Doe");
+        if (foundEmployee != null)
+        {
+            Console.WriteLine("Employee found:");
+            foundEmployee.PrintDetails();
+        }
+        else
+        {
+            Console.WriteLine("Employee not found.");
+        }
+
+        
+        List<Employee> allEmployees = database.GetAllEmployees();
+        Console.WriteLine("\nAll Employees:");
+        foreach (Employee employee in allEmployees)
+        {
+            employee.PrintDetails();
+        }
+    }
+}
