17521f31d1f7f40dfdbe6e32d7faa8b5302d7f68
Miguelitox36 <miguelitox36@hotmail.com>
2024-04-23 19:02:09 -0500

    Implementación Interfaz

---
+++ Semana 1 - Actividad 1/Semana 1 - Actividad 1/IEmployeeDatabase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_1___Actividad_1
+{
+    public class IEmployeeDatabase
+    {        
+        void AddEmployee(Employee employee);
+        Employee GetEmployeeByName(string name);
+        List<Employee> GetAllEmployees();
+    }
+}
