4f3ba918623d40482e6bdcfaeeed8cff0d0e9912
Miguelitox36 <miguelitox36@hotmail.com>
2024-04-23 19:00:55 -0500

    Implementado Polimorfismo

---
+++ Semana 1 - Actividad 1/Semana 1 - Actividad 1/Manager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_1___Actividad_1
+{
+    public class Manager : Employee
+    {
+        public string Department { get; set; }
+                
+        public Manager(string name, int age, string department) : base(name, age)
+        {
+            Department = department;
+        }
+                
+        public override void PrintDetails()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}, Department: {Department}");
+        }
+    }
+}
